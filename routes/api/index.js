f88faa332915ef7ceb0d2ab9e5e2345463b93cdd
Johan Fogelström <zapmew@gmail.com>
2017-07-11 17:53:29 +0200

    Updated speedcontrol implementation for new split on next game requirements.

--- routes/api/index.js
+++ routes/api/index.js
@@ -11,15 +11,24 @@ var app = express.Router();
 app.use(body_parser.json());
 
 app.post("/speedcontrol-event", function(req, res) {
-    var runData = req.body;
+    var eventData = req.body;
+    console.log(eventData);
     if (config.get("speedcontrol").key != undefined && 
         req.get("API-Key") !== config.get("speedcontrol").key) {
         res.status(403).json("Wrong Key");
         console.log("Blocked faulty keydata from SpeedControl. The key was ", req.get("API-key"));
         return;
     }
+    if (eventData.event == "runStarted") {
+        var runData = eventData.oldrun;
+    } else {
+        res.status(200).json("OK");
+        return;
+    }
+
     //TODO "Massage" run-data into a more convenient format.
     runData = youtube.simplify(runData);
+
 
     console.log(JSON.stringify(runData));
     if (config.has('youtube') && config.get('youtube').enable) {
@@ -41,6 +50,7 @@ var buttonInhibitor = {};
 app.post("/bigredbutton/:id", function(req, res) {
     var pressData = req.body;
     if (config.sharedkey != undefined && pressData.key !== config.get("sharedkey")) {
+        console.log("Wrong key for bigredbutton");
         res.status(403).json("Wrong Key");
         return;
     }
@@ -50,6 +60,7 @@ app.post("/bigredbutton/:id", function(req, res) {
 
     const id = parseInt(req.params.id, 10);
     if (isNaN(id) || id < 1) {
+        console.log("bigredbutton was sent a bad Id.");
         return res.status(403).json({
             message: "Bad Id.",
             id: req.params.id
@@ -57,6 +68,7 @@ app.post("/bigredbutton/:id", function(req, res) {
     }
 
     if (buttonInhibitor[id] == true) {
+        console.log("bigredbutton was sent presses too fast for a single button. Slow down!");
         return res.status(401).json({
             message: "To many presses in short time. Dropping.",
             id: id
@@ -70,12 +82,14 @@ app.post("/bigredbutton/:id", function(req, res) {
 
     speedcontrol.timers().then(function(timers) {
         if (id > timers.length) {
+            console.log("bigredbutton: Player does not exits in this run.");
             return res.status(403).json({
                 message: "Player does not exist in this run.",
                 id: id,
                 players: timers.length
             });
         }
+        console.log(timers);
         console.log(timers[id-1]);
         switch (timers[id-1].status) {
             case "waiting": return speedcontrol.start();
@@ -86,8 +100,10 @@ app.post("/bigredbutton/:id", function(req, res) {
         }
         
     }).catch(function(e) {
+        console.log("Failed to contact SpeedControl.");
         res.status(e.code || 500).json(e.message);
     }).then( function(data) {
+        console.log("Successfull call to BigRedButton for id " + id + ".");
         res.json("OK");
     });
 })
